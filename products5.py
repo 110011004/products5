6ef1f708b4989d888edaf129992c0599f097a4cc
110011004 <125714420+110011004@users.noreply.github.com>
2023-05-31 21:58:39 +0800

    Add files via upload

--- products5.py
+++ products5.py
@@ -12,6 +12,7 @@ if os.path.isfile('products.csv')：#檢查檔案在不再
 		print (products)
 	else:
 		print('找不到檔案'）
+			
 #使用者輸入
 while True:
 	name = input('請輸入商品名稱：')
@@ -21,6 +22,18 @@ while True:
 	price = int (price)
 	products. append( [name, price]) 
 print (products)
+
+#印出所有商品紀錄
+def print_products() :
+	for p in products:
+		print(p[0], '的價格是', p[1]）
+
+#寫入檔案
+def write_file():
+	with open('products.csv', 'w', encoding='utf-8') as f:
+		f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
 
 
 
